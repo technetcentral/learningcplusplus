0081fde09d260180ec82457bae5c9737964631d6
alvin@vaughns.net <alvin@vaughns.net>
2016-04-22 21:58:08 -0500

    Initial commit

---
+++ DemoCPlusPlus/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 19, 2016
+ *      Author: alvin
+ */
+//#include "StarHello.h"
+//#include "WhileLoop.h"
+#include "ForLoop.h"
+
+//WhileLoop mygrades;
+ForLoop triangle;
+
+int main () {
+
+//StarHello hello;
+//mygrades.gradCalc();
+triangle.mytriange();
+
+
+
+return 0;
+}
+
+
+
